b25ccfd35fe330d84dd8a1f0f452c663922ec7f6
Vũ Bá Quang <vubaquang1998@gmail.com>
2020-09-15 09:18:05 +0700

    First commit

--- src/main/java/com/vbqkma/libarybackend/controller/TestController.java
+++ src/main/java/com/vbqkma/libarybackend/controller/TestController.java
@@ -5,14 +5,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+
 
 @Controller
-//@RequestMapping("/rest/v1/schedules")
+@RequestMapping("/rest/vubaquang")
 public class TestController {
-
-    @GetMapping(path = "/")
+    @GetMapping(path = "/get-cookie")
     public @ResponseBody
-    ResponseEntity<byte[]> login() {
-        return new ResponseEntity<>(null, null, HttpStatus.OK);
+    ResponseEntity<String> login(@RequestParam String cookie) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("cookie.txt",true);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+            Date date = new Date();
+            bufferedWriter.write(date.toString() + ": " +cookie);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+            outputStreamWriter.close();
+            fileOutputStream.close();
+            System.out.println("Get cookie success !");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("<h1>Vũ Bá Quang - AT130444 - Get cookie success !</h1>", null, HttpStatus.OK);
     }
 }
