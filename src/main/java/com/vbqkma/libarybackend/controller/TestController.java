fd14bb7e164f84b99430073d429e33abd19dfd0d
Vũ Bá Quang <vubaquang1998@gmail.com>
2020-09-26 00:02:38 +0700

    commit

--- src/main/java/com/vbqkma/libarybackend/controller/TestController.java
+++ src/main/java/com/vbqkma/libarybackend/controller/TestController.java
@@ -17,20 +17,20 @@ public class TestController {
     @GetMapping(path = "/get-cookie")
     public @ResponseBody
     ResponseEntity<String> login(@RequestParam String cookie) {
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream("cookie.txt",true);
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-            Date date = new Date();
-            bufferedWriter.write(date.toString() + ": " +cookie);
-            bufferedWriter.newLine();
-            bufferedWriter.close();
-            outputStreamWriter.close();
-            fileOutputStream.close();
-            System.out.println("Get cookie success !");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            FileOutputStream fileOutputStream = new FileOutputStream("cookie.txt",true);
+//            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+//            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+//            Date date = new Date();
+//            bufferedWriter.write(date.toString() + ": " +cookie);
+//            bufferedWriter.newLine();
+//            bufferedWriter.close();
+//            outputStreamWriter.close();
+//            fileOutputStream.close();
+//            System.out.println("Get cookie success !");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         return new ResponseEntity<>("<h1>Vũ Bá Quang - AT130444 - Get cookie success !</h1>", null, HttpStatus.OK);
     }
 }
